e86edd2fe71649fd3e69a12cc52220b7dca36a8b
Thomas McDonald <s2951339@staff.ad.griffith.edu.au>
2018-12-19 15:41:37 +1000

    Created custom insertLines(), ng2-ace-editors version doesnt work

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -38,20 +38,22 @@ export class RoomComponent implements OnInit {
             case "insert":
                   this.silent = true
                   console.log(this.editor);
-                  if(message.lines[0] === ""){this.editor.getEditor().session.insert(message.start, '\n')}
-                  this.editor.getEditor().session.insert(message.start, message.lines[0].toString());
+
+                  if(message.lines.length > 1){
+                      if(message.lines[0] === ""){this.editor.getEditor().session.insert(message.start, '\n')}
+                      else{
+                        this.insertLines(message);
+                      }
+                  }
+                  else{
+                      this.editor.getEditor().session.insert(message.start, message.lines[0].toString());
+                  }
                   this.silent = false
                   console.log(message);
                   break;
             case "remove":
-                  console.log("Removing");
                   this.silent = true
-                  console.log(this.editor.getEditor())
-                  console.log(message)
-                
-                    this.editor.getEditor().session.remove(new Range(message.start.row,message.start.column,message.end.row,message.end.column));
-
-
+                  this.editor.getEditor().session.remove(new Range(message.start.row,message.start.column,message.end.row,message.end.column));
                   this.silent = false
                   break;
           }
@@ -69,6 +71,15 @@ export class RoomComponent implements OnInit {
     sendData(e){
       if(this.silent) return;
       this.socketService.sendData(e);
+    }
+
+    insertLines(message){
+      // This exists purely because i havent figured out how to work the insertlines function
+      for(var i=0;i<message.lines.length;i++){
+        this.editor.getEditor().session.insert(message.start, message.lines[i]);
+        message.start.row++;
+        this.editor.getEditor().session.insert(message.start, '\n');
+      }
     }
 
 
